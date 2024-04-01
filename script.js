e390e44c9c7879353ddbba38326f553c82b4ec92
Allen Mathews <allenjoemathews@gmail.com>
2024-04-01 22:28:21 +1100

    changed fonts and css

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-document.getElementById("get-activity").addEventListener("click", function() {
-    fetch("https://apis.scrimba.com/bored/api/activity")
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("activity").textContent = data.activity
-            document.getElementById("title").textContent = "🦾 HappyBot🦿"
-            document.body.classList.add("fun")
-        })
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(data => console.log(data))
 })
